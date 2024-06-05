505fc9edcbd5dd237ea2de00cb02b8a87c56e4b9
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2024-06-05 09:19:44 -0500

    update

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,6 +1,6 @@
 // services/imageService.ts
 export async function generateImage(prompt: string): Promise<string> {
-    const response = await fetch('http://localhost:8080/api/generate', {
+    const response = await fetch('https://6262-181-66-138-81.ngrok-free.app/sdapi/v1/txt2img', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -9,11 +9,12 @@ export async function generateImage(prompt: string): Promise<string> {
     });
 
     if (!response.ok) {
+        const errorText = await response.text();
+        console.error('Error response:', errorText);
         throw new Error('Fallo al generar imagen');
     }
 
     const jsonResponse = await response.json();
-    
     const base64Image = jsonResponse.images[0];
     return `data:image/png;base64,${base64Image}`;
 }
