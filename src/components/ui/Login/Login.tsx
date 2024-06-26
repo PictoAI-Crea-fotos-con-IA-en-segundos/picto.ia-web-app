22f37c3512033952bc51af3b2aec7864e93d1e5c
Frank Alva <u202020026@upc.edu.pe>
2024-06-26 02:17:25 -0500

    feat: new links

--- src/components/ui/Login/Login.tsx
+++ src/components/ui/Login/Login.tsx
@@ -20,10 +20,11 @@ const Login = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch("http://localhost:8080/auth/login", {
+      const response = await fetch("https://pictoai-backend-production.up.railway.app/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+
         },
         body: JSON.stringify({ email, password }),
       });
